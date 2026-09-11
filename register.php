a3dceab352175f16c7901b70ce9940f27c22776d
gamingfungus42 <90956255+gamingfungus42@users.noreply.github.com>
2026-09-11 19:40:53 +0800

    redirect fix

--- register.php
+++ register.php
@@ -46,7 +46,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $_SESSION['user_id'] = $pdo->lastInsertId();
         $_SESSION['username'] = $username;
 
-        header('Location: index.php');
+        header('Location: home.php');
         exit;
     }
 }
