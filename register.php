3a1a941b724bbb2b8d6ecc5a0b082d90fa32fb70
gamingfungus42 <90956255+gamingfungus42@users.noreply.github.com>
2026-09-13 12:40:15 +0800

    fixed broken home.php link in register

--- register.php
+++ register.php
@@ -46,7 +46,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $_SESSION['user_id'] = $pdo->lastInsertId();
         $_SESSION['username'] = $username;
 
-        header('Location: home.php');
+        header('Location: landing.php');
         exit;
     }
 }
